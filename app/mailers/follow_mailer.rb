dbfd93ddbfd3701ed7c18d9d1b68a5865972f751
BAROKA <birotori@gmail.com>
2020-10-28 13:47:55 +0100

    comment follow

--- app/mailers/follow_mailer.rb
+++
@@ -1,6 +0,0 @@
-class FollowMailer < ApplicationMailer
-  #def follow_mail(user)
-  #  @user = user
-  #  mail to: @user.followed.email,@user.following.email, subject: 'you have promoted an startup on bishop'
- #end
-end
